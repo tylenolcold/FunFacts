547321f23fff9165aac2a605b60dfc87100af2c7
James Lee <jameslee@jamess-mac.local>
2016-06-22 22:16:55 +0800

    Initial Commit

---
+++ FunFacts/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  FunFacts
+//
+//  Created by James Lee on 6/21/16.
+//  Copyright © 2016 James. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var funFactLabel: UILabel!
+    @IBAction func showFunFact() {
+        funFactLabel.text = "Another interesting fact!"
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        funFactLabel.text = "An interesting fact!"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
